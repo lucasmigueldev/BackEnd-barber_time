e63bdd9b07ff951a7304a29c3841b40f4d4be66d
Luquinh4449c <lucasbr8866@gmail.com>
2025-07-14 20:24:36 -0300

    :sparkles: feat: Usa credencial do Firebase por variável de ambiente

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,17 +1,7 @@
 import admin from 'firebase-admin';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+
   
-
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-
-const serviceAccount = JSON.parse(
-  fs.readFileSync(path.join(__dirname, '../config/firebaseKey.json'), 'utf8')
-);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
